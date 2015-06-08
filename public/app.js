d165da7cfa47f933efd27fc33828cc105bab84ce
Rishab <kothari7777777@gmail.com>
2015-06-08 18:54:05 +0530

    Modified layout and added click event for toolbar.

--- public/app.js
+++ public/app.js
@@ -1,12 +1,16 @@
 //app.js
 
-var app = angular.module('app',['ngMaterial']);
+var app = angular.module( 'app', [ 'ngMaterial' ] );
 
-app.config(function($mdThemingProvider){
-  $mdThemingProvider.theme('default')
-  .primaryPalette('indigo')
-  .accentPalette('pink')
-  .warnPalette('red')
-  .backgroundPalette('blue')
-  .dark()
-});
+app.config( function ( $mdThemingProvider ) {
+	$mdThemingProvider.theme( 'default' )
+		.primaryPalette( 'indigo' )
+		.accentPalette( 'pink' )
+		.warnPalette( 'red' )
+		.backgroundPalette( 'blue' )
+		.dark()
+} );
+
+app.controller( 'toolBarController', [ '$scope', function ( $scope ) {
+	$scope.showMoreTab = false;
+} ] );
